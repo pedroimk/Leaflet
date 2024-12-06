33001cb996ae8cf2062e64ebfb783d9200928adf
Pedro Morales <pedro.morales@kausana.cl>
2024-12-06 11:51:43 -0300

    working progress

--- src/layer/Tooltip.js
+++ src/layer/Tooltip.js
@@ -193,6 +193,9 @@ export var Tooltip = DivOverlay.extend({
 	},
 
 	_updatePosition: function () {
+		if(!this._map){
+			return;
+		}
 		var pos = this._map.latLngToLayerPoint(this._latlng);
 		this._setPosition(pos);
 	},
@@ -206,6 +209,9 @@ export var Tooltip = DivOverlay.extend({
 	},
 
 	_animateZoom: function (e) {
+		if(!this._map){
+			return;
+		}
 		var pos = this._map._latLngToNewLayerPoint(this._latlng, e.zoom, e.center);
 		this._setPosition(pos);
 	},
